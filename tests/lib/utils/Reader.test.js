854903727ae6b06b249398c5a867403e336f893b
Daniel Ramirez <contact@dann.dev>
2022-05-08 23:23:56 -0500

    add Test: Reader util and reader file to test

---
+++ tests/lib/utils/Reader.test.js
@@ -0,0 +1,30 @@
+const Reader = require("../../../lib/utils/Reader");
+
+describe('Test for Reader utils', () => {
+    test('Requirement 1: Test readJsonFile() method to read and get info from file ', () => {
+        const explorers = Reader.readJsonFile('./tests/reader_explorers.json');
+        expect(explorers).toStrictEqual([{
+            "name": "TestWoopa1",
+            "githubUsername": "Testjolonauta1",
+            "score": 1,
+            "mission": "node",
+            "stacks": [
+                "javascript",
+                "reasonML",
+                "elm"
+            ]
+        },
+        {
+            "name": "TestWoopa2",
+            "githubUsername": "Testjolonauta2",
+            "score": 2,
+            "mission": "node",
+            "stacks": [
+                "javascript",
+                "groovy",
+                "elm"
+            ]
+        }
+    ]);
+    });
+})
